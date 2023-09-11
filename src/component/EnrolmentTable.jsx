58d88191417546ada8b215ee7f62b90915788853
Rati3010 <mratikanta1997@gmail.com>
2023-09-11 23:43:38 +0530

    added the basic structure

---
+++ src/component/EnrolmentTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EnrolmentTable = () => {
+  return (
+    <div>EnrolmentTable</div>
+  )
+}
+
+export default EnrolmentTable
---
+++ src/component/OverViewCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OverViewCard = () => {
+  return (
+    <div>OverViewCard</div>
+  )
+}
+
+export default OverViewCard
